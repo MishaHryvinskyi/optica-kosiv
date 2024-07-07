58f6dce4d9c9fdbe393faa9736cffc096a0dfa35
Misha <hryvinskyim@gmail.com>
2024-07-07 16:52:49 +0300

    added theemes to local storage

--- src/js/theeme.js
+++ src/js/theeme.js
@@ -1,6 +1,20 @@
 const themeContainer = document.querySelector('.container-theems');
 const themeSwitch = document.getElementById('switch');
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.classList.add(savedTheme);
+        if (savedTheme === 'dark') {
+            themeSwitch.checked = true;
+        } else {
+            themeSwitch.checked = false;
+        }
+    } else {
+        document.body.classList.add('light');
+    }
+});
+
 themeContainer.addEventListener('change', onThemeChanges);
 
 function onThemeChanges(e) {
@@ -14,8 +28,10 @@ function toggleTheme(isChecked) {
     if (isChecked) {
         document.body.classList.add('dark');
         document.body.classList.remove('light');
+        localStorage.setItem('theme', 'dark');
     } else {
         document.body.classList.add('light');
         document.body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
     }
 }
