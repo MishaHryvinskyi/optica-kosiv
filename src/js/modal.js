99d055d3aaca2afc63ff87c6808396b33553d0e5
Misha <hryvinskyim@gmail.com>
2024-07-31 19:37:01 +0300

    added func escape close and close on btn click

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,30 @@
 const backdrop = document.querySelector('.backdrop');
+const modalBtn = document.querySelector('.modal-btn');
+
+modalBtn.addEventListener('click', closeModal);
+backdrop.addEventListener('click', onBackdropClick);
+document.addEventListener('keydown', onEscapeClick);
 
 function closeModal() {
-    backdrop.classList.add('is-hidden'); // Remove the dot here
-}
+    backdrop.classList.add('is-hidden'); 
+};
 
-// Correct way to add event listener
-backdrop.addEventListener('click', closeModal);
+function onBackdropClick(event) {
+    if (event.target === event.currentTarget) {
+        closeModal();
+    }
+};
+
+function onEscapeClick(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+    
+    if(isEscKey) {
+        closeModal();
+    }
+};
 
 setTimeout(() => {
     console.log("modal");
-    backdrop.classList.remove('is-hidden'); // Remove the dot here
+    backdrop.classList.remove('is-hidden'); 
 }, 2000);
